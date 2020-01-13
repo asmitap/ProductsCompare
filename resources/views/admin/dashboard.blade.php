1a0623a3289b3100b7833dc15f5b9f06caeb213d
asmita <asmitapokharel525@gmail.com>
2020-01-13 16:40:52 +0545

    dashboard designing

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,35 +1,53 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="dashboard">
-        <div class="row">
-            <div class="col-md-4 sidebar">
-                <div class="title">
-                    <h2> <a href="{{route('admindashboard')}}"> Dashboard </a> </h2>
-                </div>
-                <ul class="nav flex-column">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link active dropdown-toggle" id="dropdownMenu2" data-toggle="dropdown" href="#"> Group </a>
-                        <div class="dropdown-menu">
-                            <span class="dropdown-item-text"></span>
+<div class="dashboard">
+<div class="container-fluid">
+<div class="row">
+    <div class="col-md-4">
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+            </div>
+
+            <ul class="list-unstyled components">
+                <h2> <a href="{{route('admindashboard')}}"> Dashboard </a> </h2>
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Group</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
                             <a class="dropdown-item" href="{{route('viewgroup')}}"> View Group </a>
-                        </div>
-                    </li>
+                        </li>
+                       
+                    </ul>
+                </li>
 
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('product')}}"> Product </a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="col-md-8">
-                @yield('admin')
-            </div>
-
-        </div>
+                <li>
+                    <a class="nav-link" href="{{route('product')}}"> Product </a>
+                </li>
+            </ul>
+        </nav>
 
     </div>
-
+    </div>
+    <div class="col-md-8 dashboarda">
+    @yield('admin')
+    </div>
 </div>
+
+
+</div>    
+</div>
+<script>
+    $(document).ready(function() {
+
+        $('#sidebarCollapse').on('click', function() {
+            $('#sidebar').toggleClass('active');
+        });
+
+    });
+</script>
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
 
 
 
+
 </head>
 <body>
     <div id="app">
@@ -102,4 +103,11 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
+
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+
+
+
 </html>
